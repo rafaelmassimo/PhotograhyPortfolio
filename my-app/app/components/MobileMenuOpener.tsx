644e04d43e2e01320bb5faedfae74ff75b09a714
Rafael Massimo <rafael_massimo@hotmail.com>
2025-07-30 21:21:25 -0700

    add wave in the logo word

--- my-app/app/components/MobileMenuOpener.tsx
+++ my-app/app/components/MobileMenuOpener.tsx
@@ -6,7 +6,7 @@ const MobileMenuOpener = () => {
 	return (
 		<div className='flex flex-row items-center justify-center bob'>
 			<Image src={'/logo.png'} alt="fujifilm logo" width={70} height={40} />
-            <span className='text-[18px] ml-2 tracking-tight'>Stories</span>
+            <span className='text-[18px] ml-2 tracking-tight'>Dream</span>
 		</div>
 	);
 };
