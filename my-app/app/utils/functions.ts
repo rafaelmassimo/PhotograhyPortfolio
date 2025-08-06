49d2fa88d0fe05ebff809f0001d53f7dbd9ad677
Rafael Massimo <rafael_massimo@hotmail.com>
2025-08-05 18:33:24 -0700

    add '&' to be spaced

--- my-app/app/utils/functions.ts
+++ my-app/app/utils/functions.ts
@@ -1,9 +1,9 @@
 export const splitAndCapitalize = (input: string) => {
-	// Insert space before uppercase letters (except the first char)
-	const withSpaces = input.replace(/([A-Z])/g, ' $1').trim();
+	// Insert space before uppercase letters and & symbol (except the first char)
+	const withSpaces = input.replace(/([A-Z&])/g, ' $1').trim();
 
 	// Capitalize first letter of each word
-	const words = withSpaces.split(' ').map((word) => {
+	const words = withSpaces.split(' ').map((word, i) => {
 		return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
 	});
 
